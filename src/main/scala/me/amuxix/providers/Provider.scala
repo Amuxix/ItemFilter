08e628331498cd0a8c28ba8aa7d60df60b9ecc47
Samuel Martins <samuel.martins@enear.co>
2018-09-12 15:02:43 +0100

    Another attempt at a cache

--- src/main/scala/me/amuxix/providers/Provider.scala
+++ src/main/scala/me/amuxix/providers/Provider.scala
@@ -3,12 +3,18 @@ package me.amuxix.providers
 import cats.data.{EitherT, OptionT}
 import me.amuxix.League
 import me.amuxix.items.Item
+import me.amuxix.providers.Provider.requestsInProgress
 import me.amuxix.providers.poeninja.PoeNinja.defaultLeague
 import play.api.libs.json.{JsValue, Reads}
 import play.api.libs.ws.JsonBodyReadables._
 import play.api.libs.ws.{StandaloneWSClient, StandaloneWSResponse}
 
+import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
+
+object Provider {
+  private def requestsInProgress = mutable.Map.empty[(String, Seq[(String, String)]), Future[Either[ProviderError, Any]]]
+}
 
 abstract class Provider(wsClient: StandaloneWSClient)(implicit ec: ExecutionContext) {
   implicit class ParsableWSResponse(response: StandaloneWSResponse) {
@@ -28,14 +34,20 @@ abstract class Provider(wsClient: StandaloneWSClient)(implicit ec: ExecutionCont
       }
   }
 
-  protected def get[Response](url: String, parameters: (String, String)*)(implicit reads: Reads[Response]): EitherT[Future, ProviderError, Response] =
-    EitherT(
+  protected def get[Response](url: String, parameters: (String, String)*)(implicit reads: Reads[Response]): EitherT[Future, ProviderError, Response] = {
+    val future = requestsInProgress.getOrElseUpdate((url, parameters), {
+      println("making new request")
+      println(requestsInProgress)
       wsClient
         .url(url)
         .withQueryStringParameters(parameters.toSeq: _*)
         .get()
         .map(_.parse[Response](url))
-    )
+    }
+    ).map(_.map(_.asInstanceOf[Response]))
+    future.onComplete(_ => requestsInProgress.remove((url, parameters)))
+    EitherT(future)
+  }
 
   def getChaosEquivalentFor(item: Item, league: League = defaultLeague): OptionT[Future, Double]
 }
