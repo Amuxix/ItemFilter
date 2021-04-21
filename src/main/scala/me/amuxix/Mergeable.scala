1595d6f464787db4c5318bca361b49067a1e6a8c
Amuxix <amuxix@gmail.com>
2021-04-21 03:07:56 +0100

    Use Mergeable syntax

--- src/main/scala/me/amuxix/Mergeable.scala
+++ src/main/scala/me/amuxix/Mergeable.scala
@@ -3,16 +3,17 @@ package me.amuxix
 import cats.data.NonEmptyList
 import cats.effect.IO
 import cats.syntax.parallel._
+import me.amuxix.syntax.mergeable._
 
 import scala.annotation.tailrec
 
 object Mergeable {
-  @tailrec def merge[T](items: NonEmptyList[T], merged: List[T] = List.empty)(implicit M: Mergeable[T]): NonEmptyList[T] =
+  @tailrec def merge[T: Mergeable](items: NonEmptyList[T], merged: List[T] = List.empty): NonEmptyList[T] =
     items match {
       case NonEmptyList(first, rest) =>
         val (mergedItem, unmergeable) = rest.foldLeft((first, List.empty[T])) {
-          case ((mergedItem, unmergeable), item) if M.canMerge(mergedItem, item) =>
-            (M.merge(mergedItem, item), unmergeable)
+          case ((mergedItem, unmergeable), item) if mergedItem canMerge item =>
+            (mergedItem merge item, unmergeable)
           case ((mergedItem, unmergeable), item) =>
             (mergedItem, unmergeable :+ item)
         }
@@ -24,16 +25,16 @@ object Mergeable {
         }
     }
 
-  def parMerge[T](items: NonEmptyList[T])(implicit M: Mergeable[T]): IO[NonEmptyList[T]] = {
+  def parMerge[T: Mergeable](items: NonEmptyList[T]): IO[NonEmptyList[T]] = {
     @tailrec def groupByMergeables(remainingItems: NonEmptyList[T], groups: List[NonEmptyList[T]] = List.empty): List[NonEmptyList[T]] = remainingItems match {
       case NonEmptyList(first, rest) =>
-        rest.partition(M.canMerge(first, _)) match {
+        rest.partition(first.canMerge) match {
           case (mergeables, List()) => groups :+ NonEmptyList.of(first, mergeables: _*)
           case (mergeables, List(unmergeable)) => groups :+ NonEmptyList.of(first, mergeables: _*) :+ NonEmptyList.one(unmergeable)
           case (mergeables, unmergeables) => groupByMergeables(NonEmptyList.fromListUnsafe(unmergeables), groups :+ NonEmptyList.of(first, mergeables: _*))
         }
     }
-    def merge(items: NonEmptyList[T]): IO[T] = IO(items.tail.fold(items.head)(M.merge))
+    def merge(items: NonEmptyList[T]): IO[T] = IO(items.tail.fold(items.head)(_ merge _))
 
     groupByMergeables(items).parTraverse(merge).map(NonEmptyList.fromListUnsafe)
   }
